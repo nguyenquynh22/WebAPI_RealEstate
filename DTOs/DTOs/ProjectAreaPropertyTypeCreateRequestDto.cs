313a1a8aa38d1a5f28600ac93461741a6e789a5b
nguyenquynh22 <quynhng827@gmail.com>
2026-01-02 14:57:16 +0700

    create dto projecttype

---
+++ DTOs/DTOs/ProjectAreaPropertyTypeCreateRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_DTOs.DTOs
+{
+    public class ProjectAreaPropertyTypeCreateRequestDto
+    {
+        public Guid AreaId { get; set; }
+        public int PropertyTypeId { get; set; }
+    }
+}
