a3b80d61b5daba6938d5916b4c3917a568516a8a
Thu Huong Hoang <1707huong@gmail.com>
2026-01-02 23:50:10 +0700

    update

---
+++ DTOs/DTOs/NewsFilterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Common_DTOs.DTOs
+{
+    public class NewsFilterDto
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        public string Status { get; set; } = string.Empty;  // "" => all
+        public string Keyword { get; set; } = string.Empty; // "" => all
+        public int IsHighlight { get; set; } = -1;          // -1 all, 0 false, 1 true
+        public int IsExternal { get; set; } = -1;           // -1 all, 0 false, 1 true
+    }
+}
