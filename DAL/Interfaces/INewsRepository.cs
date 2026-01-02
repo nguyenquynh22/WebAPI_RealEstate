8c3416c800a33820d6675bd232bd41d31ca61cc8
Thu Huong Hoang <1707huong@gmail.com>
2026-01-02 23:51:17 +0700

    update

---
+++ DAL/Interfaces/INewsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common_DTOs.DTOs;
+
+namespace Common_DAL.Interfaces
+{
+    public interface INewsRepository
+    {
+        Task<Guid> CreateAsync(NewsCreateRequestDto dto);
+        Task<NewsResponseDto> GetByIdAsync(Guid newsId);
+        Task<bool> UpdateAsync(NewsUpdateRequestDto dto);
+        Task<bool> DeleteAsync(Guid newsId);
+        Task<(List<NewsResponseDto> Items, int Total)> GetListAsync(NewsFilterDto filter);
+    }
+}
