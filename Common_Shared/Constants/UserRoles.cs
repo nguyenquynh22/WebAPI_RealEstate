cc3f61f00a75251bea50c9bb02c6e75c45f407f4
hiudz377 <hieutieunhan6@gmail.com>
2026-01-04 20:19:31 +0700

    Revert "done_crud"
    This reverts commit 82278134b32aad3173c98d565c37db56d4ded67c.

---
+++ Common_Shared/Constants/UserRoles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Shared.Constants
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string Agent = "Agent";
+        public const string AdminOrAgent = "Admin,Agent";
+    }
+}
