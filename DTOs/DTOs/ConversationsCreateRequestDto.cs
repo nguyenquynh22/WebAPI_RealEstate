623959550e5775e31cae0a908db0c25d8117997d
Thu Huong Hoang <1707huong@gmail.com>
2026-01-04 10:38:21 +0700

    dto Conversations

---
+++ DTOs/DTOs/ConversationsCreateRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Common_DTOs.DTOs
+{
+    public class ConversationsCreateRequestDto
+    {
+        public Guid Participant1Id { get; set; }
+        public Guid Participant2Id { get; set; }
+    }
+}
